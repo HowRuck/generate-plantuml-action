ce57c728b9fab217e300f57029cd8729afa33215
Erik <44932832+HowRuck@users.noreply.github.com>
2024-03-20 10:50:35 +0000

    Fixed incorrect URL

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ type GenerateSvgPayload = {
 
 async function generateSvg(payload: GenerateSvgPayload) {
   const encoded = plantumlEncoder.encode(payload.code)
-  let url = `http://www.plantuml.com/plantuml/svg/${encoded}`
+  let url = `http://www.plantuml.com/plantuml/svg/~1${encoded}`
   let headers = {}
 
   if (payload.server) {
